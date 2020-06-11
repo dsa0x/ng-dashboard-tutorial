57215269961c7b3a6a9dc00521f219996bae145a
Dare Samsondeen <dsamsondeen@gmail.com>
2020-06-11 14:39:31 +0200

    dashboard code along

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AreaComponent } from './widgets/area/area.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { CardComponent } from './widgets/card/card.component';
+import { PieComponent } from './widgets/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    AreaComponent,
+    CardComponent,
+    PieComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule,
+    HighchartsChartModule,
+  ],
+
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    AreaComponent,
+    CardComponent,
+    PieComponent,
+  ],
+})
+export class SharedModule {}
---
+++ src/app/shared/widgets/area/area.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-area',
+  templateUrl: './area.component.html',
+  styleUrls: ['./area.component.scss'],
+})
+export class AreaComponent implements OnInit {
+  chartOptions = {};
+
+  @Input() data = [];
+
+  Highcharts = Highcharts;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+      },
+      title: {
+        text: 'Random Data',
+      },
+      subtitle: {
+        text: 'Demo',
+      },
+
+      tooltip: {
+        split: true,
+        valueSuffix: ' millions',
+      },
+      exporting: {
+        enabled: true,
+      },
+      credits: {
+        enabled: false,
+      },
+      series: this.data,
+    };
+
+    HC_exporting(this.Highcharts);
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 300);
+  }
+}
